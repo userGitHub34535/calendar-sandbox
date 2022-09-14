7891e512ca9365bc1a1fa416e8b573df8eda2f93
sheila <sheilasubbiah3@gmail.com>
2022-09-14 14:55:15 -0500

    found a React date-time input that I like

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import DateTimePicker from 'react-datetime-picker';
+import TimePicker from 'react-time-picker';
 
 function App() {
   return (
     <div className="App">
       <input type="time" />
+      <DateTimePicker  autoFocus={true} format="MM-dd-y HH:mm" value={new Date(Date.now())}/>
+      <DateTimePicker  autoFocus={true} format="MM-dd-y HH:mm" value={new Date(Date.now())}/>
     </div>
   );
 }
