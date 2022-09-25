58a164f48f7461efa42127c894374c3d2ac530c1
sheila <sheilasubbiah3@gmail.com>
2022-09-24 22:10:05 -0500

    calendar-wk-view & month-vw update simultaneously

--- src/Components/CalendarHeader.js
+++ src/Components/CalendarHeader.js
@@ -3,41 +3,40 @@ import GlobalContext from "../Context/GlobalContext";
 
 export default function Header() {
     const {monthIndex, setMonthIndex, weekIndex, setWeekIndex} = useContext(GlobalContext);
+    
+    function handlePrev() {
+        handlePrevMonth();
+        handlePrevWeek();
+    }
+    function handleNext() {
+        handleNextMonth();
+        handleNextWeek();
+    }
+
     function handlePrevMonth() {
         setMonthIndex(monthIndex - 1);
-    }
-    function handleNextMonth() {
-        setMonthIndex(monthIndex + 1);
     }
     function handlePrevWeek() {
         setWeekIndex(weekIndex - 1);
     }
+    function handleNextMonth() {
+        setMonthIndex(monthIndex + 1);
+    }
     function handleNextWeek() {
         setWeekIndex(weekIndex + 1);
     }
+
     return (
         <header>
             <h1 className="mr-10 text-xl text-gray-500 font-bold">
                 Clock My Day
             </h1>
-            <button onClick={handlePrevWeek
-            }
-                // () => {
-                // this.handlePrevMonth(); 
-                // this.handlePrevWeek();
-                // }}
-                >
+            <button onClick={handlePrev}>
                 <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
                     {`<`}
                 </span>
             </button>
-            <button onClick={handleNextWeek
-            }
-                // () => {
-                // this.handleNextMonth(); 
-                // this.handleNextWeek();
-                // }}
-                >
+            <button onClick={handleNext}>
                 <span className="cursor-pointer mx-2">
                     {`>`}
                 </span>
